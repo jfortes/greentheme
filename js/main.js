aab5c995ef624c725eee790ae18da6ee553cb1c0
unknown <jamal.fortes@44393-WIN-501.auckland.natcoll.ac.nz>
2014-11-07 14:55:21 +1300

    second

--- js/main.js
+++ js/main.js
@@ -1,6 +1,38 @@
-$(function(){
-	$("#header-menu").on("click",function(){
-		$("#header ul").toggleClass("show");
-	});
+// $(function(){
+// 	$("#header-menu").on("click",function(){
+// 		$("#header ul").toggleClass("show");
+// 	});
 	
+// });
+
+
+//-------------- accordian bit
+
+$(function(){
+
+	$("#main-side-selection h2").on("click",function(){
+
+		$(this).next().prepareTransition().toggleClass("hide");
+
+	});
+
+	// $("#main-side-selection p").on("transitionend",function(){
+
+	// 	if($(this).hasClass("hide") == false){
+
+	// 		$(this).prev().children("i")
+	// 			.removeClass("fa fa-leaf")
+	// 			.addClass("fa fa-leaf");
+
+	// 	}else{
+
+	// 		$(this).prev().children("i")
+	// 			.removeClass("fa fa-leaf")
+	// 			.addClass("fa fa-leaf");
+	// 	}
+
+	// });
+
 });
+
+
