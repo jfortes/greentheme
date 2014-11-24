eecb6a584215db1d331514e019ea52ffe2daefba
unknown <jamal.fortes@44393-WIN-501.auckland.natcoll.ac.nz>
2014-11-24 14:56:02 +1300

    front almost finish

--- js/main.js
+++ js/main.js
@@ -30,183 +30,190 @@ $(function(){
 
 	});
 
+
     $(".blogs-link>a").on("click", function(e){
         e.preventDefault();
         $(this).parent().prev().toggleClass("show");
     });
 
 	
-});
 
 
-var styles = [
-    {
-        "featureType": "landscape",
-        "stylers": [
+    if($("#greentheme-map").length > 0){
+
+        var styles = [
             {
-                "saturation": -100
+                "featureType": "landscape",
+                "stylers": [
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 65
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
             },
             {
-                "lightness": 65
+                "featureType": "poi",
+                "stylers": [
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 51
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
             },
             {
-                "visibility": "on"
+                "featureType": "road.highway",
+                "stylers": [
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "stylers": [
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 30
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "stylers": [
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 40
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "stylers": [
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.province",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "lightness": -25
+                    },
+                    {
+                        "saturation": -100
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "hue": "#ffff00"
+                    },
+                    {
+                        "lightness": -25
+                    },
+                    {
+                        "saturation": -97
+                    }
+                ]
             }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 51
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 30
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 40
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative.province",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "lightness": -25
-            },
-            {
-                "saturation": -100
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "hue": "#ffff00"
-            },
-            {
-                "lightness": -25
-            },
-            {
-                "saturation": -97
-            }
-        ]
+        ];
+
+
+        function initialize(){
+            var mapOptions = {
+                zoom: 15,
+                center: new google.maps.LatLng(-36.767163, 174.755491),
+                styles: styles
+            };
+            map = new google.maps.Map(document.getElementById('greentheme-map'), mapOptions);
+
+        }
+
+        google.maps.event.addDomListener(window,'load',initialize);
+
+
+
+        window.onload = function(){
+
+        	var request = {address: "102B East Coast Road, Forrest Hill"};
+        	var geocoder = new google.maps.Geocoder();
+        	geocoder.geocode(request,function(results,status){
+
+        		if(status == "OK"){
+        			var destination = results[0].geometry.location;
+        			map.setCenter(destination);
+
+        			//destination marker
+        			var destinationMarker = new google.maps.Marker({map:map});
+        			destinationMarker.setPosition(destination);
+
+        			var destinationInfo = new google.maps.InfoWindow({content:'<p class="address">Greentheme Services</p> <p>102B East Coast Road, Forrest Hill, Auckland 0620</p><p>Phone: 0800 399 225</p>'});
+        				destinationInfo.open(map,destinationMarker);
+
+        			if(geo_position_js.init()){
+
+        			   geo_position_js.getCurrentPosition(function(position){
+        			   		var origin =  new google.maps.LatLng(position.coords.latitude, 
+        						   											position.coords.longitude);
+        			   		var originMarker = new google.maps.Marker({map:map});
+        					originMarker.setPosition(origin);
+
+
+
+        			   },function(error){
+        			   		console.log(error)
+
+        			   });
+        			}else{
+        				console.log("this feature is not supported");
+        			}
+        						   	
+        		}
+        	});
+        };
+
     }
-];
 
-
-function initialize(){
-	var mapOptions = {
-		zoom: 15,
-		center: new google.maps.LatLng(-36.767163, 174.755491),
-		styles: styles
-	};
-	map = new google.maps.Map(document.getElementById('greentheme-map'), mapOptions);
-
-}
-
-google.maps.event.addDomListener(window,'load',initialize);
-
-window.onload = function(){
-
-	var request = {address: "102B East Coast Road, Forrest Hill"};
-	var geocoder = new google.maps.Geocoder();
-	geocoder.geocode(request,function(results,status){
-
-		if(status == "OK"){
-			var destination = results[0].geometry.location;
-			map.setCenter(destination);
-
-			//destination marker
-			var destinationMarker = new google.maps.Marker({map:map});
-			destinationMarker.setPosition(destination);
-
-			var destinationInfo = new google.maps.InfoWindow({content:'<p class="address">Greentheme Services</p> <p>102B East Coast Road, Forrest Hill, Auckland 0620</p><p>Phone: 0800 399 225</p>'});
-				destinationInfo.open(map,destinationMarker);
-
-			if(geo_position_js.init()){
-
-			   geo_position_js.getCurrentPosition(function(position){
-			   		var origin =  new google.maps.LatLng(position.coords.latitude, 
-						   											position.coords.longitude);
-			   		var originMarker = new google.maps.Marker({map:map});
-					originMarker.setPosition(origin);
-
-
-
-			   },function(error){
-			   		console.log(error)
-
-			   });
-			}else{
-				console.log("this feature is not supported");
-			}
-						   	
-		}
-	});
-};
-
+});
